f1df08a973f8a4a2847f1d2095b3ae0c78367eda
Bruno Jutkoski <bjutkoski@MacBook-Pro-de-Bruno.local>
2020-02-26 22:04:42 -0300

    Testando formulário

--- src/__tests__/components/TechList.test.js
+++ src/__tests__/components/TechList.test.js
@@ -1,18 +1,16 @@
-import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 
-import TechList from "~/components/TechList";
+import TechList from '~/components/TechList';
 
-describe("TechList component", () => {
-  it("should be able to add new tech", () => {
-    const { getByText, getByTestId, debug } = render(<TechList />);
+describe('TechList component', () => {
+  it('should be able to add new tech', () => {
+    const { getByText, getByTestId, getByLabelText } = render(<TechList />);
 
-    debug();
+    fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
+    fireEvent.submit(getByTestId('tech-form'));
 
-    fireEvent.click(getByText("Adicionar"));
-
-    debug();
-
-    expect(getByTestId("tech-list")).toContainElement(getByText("Node.js"));
+    expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+    expect(getByLabelText('Tech')).toHaveValue('');
   });
 });
